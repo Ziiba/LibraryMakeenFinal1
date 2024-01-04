3ab3df2f9f04b25ac8b3944c9a28f877886bb4da
ziba saberi <zibasaberi400@gmail.com>
2024-01-04 11:41:23 +0330

    Initial commit

---
+++ src/main/java/com/example/librarymakeenfinal/LibraryMakeenFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarymakeenfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMakeenFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryMakeenFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/librarymakeenfinal/LibraryMakeenFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarymakeenfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMakeenFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
